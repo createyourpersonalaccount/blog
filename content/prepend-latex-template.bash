f3b82f9ecba670deb0f6336e40ba5263e5ce87f7
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2025-01-16 22:59:22 -0500

    include unlimited headings to table of contents

--- content/prepend-latex-template.bash
+++ content/prepend-latex-template.bash
@@ -6,7 +6,7 @@ for path in "$@"; do
     for ((i = 0; i < depth; ++i)); do
         dots="../${dots}"
     done
-    awkstr=$(printf '{if ($0 !~ /^#\+/ && !b) {print "#+INCLUDE: %s%s"; print "#+BIBLIOGRAPHY: %s%s"; print "#+CITE_EXPORT: csl %s%s"; print $0; b=1;} else {print $0} }' "$dots" "latex-template.org" "$dots" "bibliography.bib" "$dots" "elsevier-with-titles.csl")
+    awkstr=$(printf '{if ($0 !~ /^#\+/ && !b) {print "#+OPTIONS: H:99"; print "#+INCLUDE: %s%s"; print "#+BIBLIOGRAPHY: %s%s"; print "#+CITE_EXPORT: csl %s%s"; print $0; b=1;} else {print $0} }' "$dots" "latex-template.org" "$dots" "bibliography.bib" "$dots" "elsevier-with-titles.csl")
     awk "$awkstr" "$path" > "${path}.tmp"
     mv "${path}.tmp" "$path"
 done
